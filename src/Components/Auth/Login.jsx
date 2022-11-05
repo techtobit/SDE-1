dc207a00d1ed82d98f79462a944b658317c18001
MD ASHRAF UDDIN <43129513+techtobit@users.noreply.github.com>
2022-11-05 19:31:24 +0600

    spa-setup

---
+++ src/Components/Auth/Login.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Login = () => {
+ return (
+  <div>
+   <div className="left-side">
+    <h2>Welcome to Rsquare.</h2>
+   </div>
+   <div className="right-side">
+    <h2>Create an account</h2>
+   </div>
+  </div>
+ );
+};
+
+export default Login;
